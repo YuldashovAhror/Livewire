e81128202e4555db60f86123a821147963356286
Yuldashov Ahror <ahroryuldashov123gmail.com>
2022-06-28 18:32:15 +0500

    style comment

--- app/Http/Livewire/Comments.php
+++ app/Http/Livewire/Comments.php
@@ -3,7 +3,7 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
-
+use Carbon\Carbon;
 class Comments extends Component
 {
     public $comments = [
@@ -11,12 +11,30 @@ class Comments extends Component
             'body' =>'Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                      Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, 
                      when an unknown printer took a galley of type and scrambled it to make a type specimen book',
-
-            'created_ad' => '3 min ago',
-            
+            'created_at' => '3 min ago',
             'creator' => 'Ahror'
         ]
     ];
+
+    public $newComment;
+
+    public function mount()
+    {
+        $this->newComment = 'I am from function';
+    }
+
+    public function addComment()
+    {
+        if($this->newComment == '')
+         return;
+        array_unshift($this->comments,[
+            'body' =>$this->newComment,
+            'created_at' => '1 min ago',
+            'creator' => 'Abror'
+        ]);
+
+        $this->newComment = '';
+    }
 
     public function render()
     {
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,13 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
     <title>Laravel Livewire</title>
     @livewireStyles
 </head>
 <body>
     
     {{-- {{ $slot }} --}}
-    @livewire('counter')
+    {{-- @livewire('counter') --}}
+    @livewire('comments')
     @livewireScripts
 </body>
 </html>
